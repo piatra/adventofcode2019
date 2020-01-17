a5a72ddae1424b85908b45edfb85add608255de5
Andrei Oprea <andrei.br92@gmail.com>
2020-01-17 17:47:43 +0100

    Updates

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,7 +1,29 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-fn eval(input: &mut Vec<u32>) -> () {
+fn compute(opcode: i32, in1: i32, in2: i32) -> i32 {
+    match opcode {
+        1 => in1 + in2,
+        2 => in1 * in2,
+        _ => {
+            println!("Unknown op {}", opcode);
+            0
+        },
+    }
+}
+
+fn mode_to_value(input: &mut Vec<i32>, mode: i32, param: i32) -> i32 {
+    match mode {
+        0 => input[param as usize],
+        1 => param,
+        _ => {
+            println!("Unknown mode {}", mode);
+            0
+        },
+    }
+}
+
+fn eval(input: &mut Vec<i32>) -> () {
     let mut curr = 0;
     loop {
         match input[curr] {
@@ -21,10 +43,31 @@ fn eval(input: &mut Vec<u32>) -> () {
                 println!("{} * {} to {}", input[in1], input[in2], input[out]);
                 curr += 3;
             }
+            3 => {
+                let out = input[curr + 1] as usize;
+                curr += 1;
+            }
+            4 => {
+                let out = input[curr + 1] as usize;
+                curr += 1;
+            }
             99 => {
                 break;
             }
-            _ => curr += 1
+            _ => {
+                let opcode = input[curr] % 100;
+                let mode1 = (input[curr] % 1000 - opcode) / 100;
+                let mode2 = (input[curr] % 10000 - opcode - mode1) / 1000;
+                let mode3 = (input[curr] % 100000 - opcode - mode1 - mode2) / 10000;
+                println!("opcode = {}, mode1 = {}, mode2 = {}, mode3 = {}", opcode, mode1, mode2, mode3);
+                let in1 = mode_to_value(input, mode1, input[curr + 1]);
+                let in2 = mode_to_value(input, mode2, input[curr + 2]);
+                let out = mode_to_value(input, mode3, input[curr + 3]) as usize;
+                println!("retrieved {} in mode {} from {}", out, mode3, input[curr + 3]);
+                println!("in1 = {}, in2 = {}, out={}", in1, in2, out);
+                input[out] = compute(opcode, in1, in2);
+                curr += 1;
+            }
         }
     }
 }
@@ -34,30 +77,26 @@ fn main() -> std::io::Result<()> {
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    let mut input: Vec<u32>  = contents.split(",")
+    let mut input: Vec<i32>  = contents.split(",")
         .filter(|x| x.len() > 0)
-        .map(|x| x.trim().parse::<u32>().unwrap())
+        .map(|x| x.trim().parse::<i32>().unwrap())
         .collect();
 
     eval(&mut input);
-
-    println!("Total = {:?}", input);
-    let diff: i32 = 19690720 - (input[0] as i32);
-    println!("diff = {}", diff);
 
     Ok(())
 }
 
 #[test]
 fn test_add() {
-    let mut input: Vec<u32> = vec![1, 1, 2, 0, 99];
+    let mut input: Vec<i32> = vec![1, 1, 2, 0, 99];
     eval(&mut input);
     assert_eq!(input, vec![3, 1, 2, 0, 99]);
 }
 
 #[test]
 fn test_mul() {
-    let mut input: Vec<u32> = vec![2, 2, 2, 0, 99];
+    let mut input: Vec<i32> = vec![2, 2, 2, 0, 99];
     eval(&mut input);
     assert_eq!(input, vec![4, 2, 2, 0, 99]);
 }
